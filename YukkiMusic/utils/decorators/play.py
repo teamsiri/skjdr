5ed696f106defed7d7e700148637cf92e0f3db40
teamsiri <siribot466@gmail.com>
2024-06-04 02:40:57 +0300

    Update play.py

--- YukkiMusic/utils/decorators/play.py
+++ YukkiMusic/utils/decorators/play.py
@@ -161,7 +161,7 @@ def PlayWrapper(command):
                     invitelink = invitelink.replace(
                         "https://t.me/+", "https://t.me/joinchat/"
                     )
-                myu = await message.reply_text(f"جاري الانضمام حساب المساعد  ...")
+                myu = await message.reply_text(f"")
                 try:
                     await asyncio.sleep(1)
                     await userbot.join_chat(invitelink)
