e5f569c13cff42e37b6d2b6cd5d58e60da0e883e
teamsiri <siribot466@gmail.com>
2024-06-07 01:52:08 +0300

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -9,7 +9,7 @@
 #
 
 import sys
-
+from import config LOG_GROUP_ID
 from pyrogram import Client
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import BotCommand
@@ -20,9 +20,9 @@ from ..logging import LOGGER
 
 
 class YukkiBot(Client):
-    def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot")
-        super().__init__(
+    def init(self):
+        LOGGER(name).info(f"Starting Bot")
+        super().init(
             "YukkiMusicBot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
@@ -40,12 +40,11 @@ class YukkiBot(Client):
         self.mention = self.me.mention
 
         try:
-            await self.send_message(
-                config.LOG_GROUP_ID,
+            await self.send_message(LOG_GROUP_ID,
                 text=f"<u><b>{self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
             )
         except:
-            LOGGER(__name__).error(
+            LOGGER(name).error(
                 "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
             )
             sys.exit()
@@ -75,12 +74,12 @@ class YukkiBot(Client):
                 pass
         else:
             pass
-        a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
+        a = await self.get_chat_member(LOG_GROUP_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
-            LOGGER(__name__).error("Please promote Bot as Admin in Logger Group")
+            LOGGER(name).error("Please promote Bot as Admin in Logger Group")
             sys.exit()
         if get_me.last_name:
             self.name = get_me.first_name + " " + get_me.last_name
         else:
             self.name = get_me.first_name
-        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+        LOGGER(name).info(f"MusicBot Started as {self.name}")
